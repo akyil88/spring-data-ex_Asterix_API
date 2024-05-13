1b60fc2bcac83744a68b87f83ae7448e860d37b6
Samuel Gesang <sgesang@mailo.com>
2024-05-13 11:23:26 +0200

    Initial commit

---
+++ src/test/java/org/example/springdataex_asterix_api/SpringDataExAsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdataex_asterix_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataExAsterixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
